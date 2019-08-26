5f6808272fd3c016cf830e01c689076fdb7df01b
MeganeComputer <megane.computer@icloud.com>
2019-08-27 02:13:12 +0900

    update

---
+++ SDK/CheckBoxClass/CheckboxButton.h
@@ -0,0 +1,16 @@
+//
+//  CheckboxButton.h
+//  Alarm
+//
+//  Created by Megane.computer on 2016/12/26.
+//  Copyright (c) 2014年 Meganecomputer. All rights reserved.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CheckboxButton : UIButton
+
+@property (nonatomic, assign) BOOL checkBoxSelected;
+
+@end
